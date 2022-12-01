ccc0e378b1b52e687bc68760a8b9690cc07344c9
Kashiff <kemist88@gmail.com>
2022-12-01 09:14:21 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("welcome to new class = AlgoRiddim");
+
+//create a funtion that takes in an argument and exicutes the console log method
+
+//
+function cLog(consolelogOut) {
+  console.log(consolelogOut);
+}
+cLog("AlgoRiddim...Loaded");
